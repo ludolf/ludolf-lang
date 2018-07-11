e12156bb8b9cc8dfc7634baf46528f8cb407a0a8
Tomas Tulka <tomas.tulka@gmail.com>
2018-07-11 14:34:22 +0200

    if function

--- src/ludolf-lang.js
+++ src/ludolf-lang.js
@@ -1,3 +1,7 @@
-function bbbbbb() {
-  console.log('bbbbbbbbbb')
+/**
+ * The else part is mandatory because the function must always return a value.
+ */
+$if = (condition, then, otherwise) => {
+  const cond = ('function' === typeof condition) ? condition() : cond;
+  return cond ? then : otherwise;
 }
