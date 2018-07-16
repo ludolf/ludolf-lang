005563cf2af6529c84bd80ff5261e67d4dc9ff6c
Tomas Tulka <tomas.tulka@gmail.com>
2018-07-16 20:02:38 +0200

    repeat added

--- src/ludolf-lang.js
+++ src/ludolf-lang.js
@@ -1,4 +1,6 @@
 /**
+ * Condition function.
+ *   
  * The else part is mandatory because the function must always return a value.
  * The value is returned as added in parameters `then` and `otherwise`.
  * 
@@ -14,9 +16,22 @@
  * 
  * @param condition - the condition, if the condition is a function, the condition is the result of the function call
  * @param then - to return if the condition is true
- * @param otherwise - to return otherwise
+ * @param otherwise - to return otherwise  
  */
 const $if = (condition, then, otherwise) => {
   const cond = ('function' === typeof condition) ? condition() : condition;
   return cond ? then : otherwise;
 }
+
+/**
+ * Repeat an item function.
+ * 
+ * @param count - the count of repetition
+ * @param item - the item to be repeated
+ * @return an array of items in length of `count`
+ */    
+const $repeat = (count, item) => {
+  const arr = [];
+  arr.length = parseInt(count);
+  return arr.fill(item);
+} 
